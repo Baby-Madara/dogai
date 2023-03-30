de3a8080236c4e3318094823cd5c25c742623095
baby-madara <ahmedfarahat1177@gmail.com>
2023-03-30 13:53:06 +0200

    first dogai git trial

---
+++ launch/dogai.launch.py
@@ -0,0 +1,13 @@
+from launch             import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_decription():
+    return LaunchDescription([
+        Node(
+			package 	= '',
+			executable 	= '',
+        
+		)
+	])
+
